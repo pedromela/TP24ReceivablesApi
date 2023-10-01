7ba973fdd372276b81973c35b716cb087c6bb229
pedromela <pedro.miguel.69@hotmail.com>
2023-10-01 16:34:52 +0100

    Project with implementation to store Receivables in local sqlite database

---
+++ TP24Persistence/Migrations/20231001032421_initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TP24Entities.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Receivables",
+                columns: table => new
+                {
+                    Reference = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "TEXT", nullable: false),
+                    IssueDate = table.Column<string>(type: "TEXT", nullable: false),
+                    OpeningValue = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PaidValue = table.Column<decimal>(type: "TEXT", nullable: false),
+                    DueDate = table.Column<string>(type: "TEXT", nullable: false),
+                    ClosedDate = table.Column<string>(type: "TEXT", nullable: true),
+                    Cancelled = table.Column<bool>(type: "INTEGER", nullable: true),
+                    DebtorName = table.Column<string>(type: "TEXT", nullable: false),
+                    DebtorReference = table.Column<string>(type: "TEXT", nullable: false),
+                    DebtorAddress1 = table.Column<string>(type: "TEXT", nullable: true),
+                    DebtorAddress2 = table.Column<string>(type: "TEXT", nullable: true),
+                    DebtorTown = table.Column<string>(type: "TEXT", nullable: true),
+                    DebtorState = table.Column<string>(type: "TEXT", nullable: true),
+                    DebtorZip = table.Column<string>(type: "TEXT", nullable: true),
+                    DebtorCountryCode = table.Column<string>(type: "TEXT", nullable: false),
+                    DebtorRegistrationNumber = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receivables", x => x.Reference);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Receivables");
+        }
+    }
+}
---
+++ TP24Persistence/ReceivablesContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TP24Entities.Models;
+
+namespace TP24Entities
+{
+
+    public class ReceivablesContext : DbContext
+    {
+        public ReceivablesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Receivable> Receivables { get; set; }
+    }
+}
