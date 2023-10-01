3b508dcd9f91f77137a6f8450c59fcd579ec1f5b
pedromela <pedro.miguel.69@hotmail.com>
2023-10-01 16:36:40 +0100

    Custom exception handling

---
+++ TP24LendingApi/CustomExceptionMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+using TP24LendingApi.Models;
+
+namespace TP24LendingApi.CustomExceptionMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception)
+            {
+                await HandleExceptionAsync(httpContext, "Internal Server Error.");
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = message
+            }.ToString());
+        }
+    }
+}
